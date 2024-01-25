d9b03de9bc8e3ef54f7f6d490902c092bf07f5da
RFLXN <solid2113@naver.com>
2024-01-25 21:01:24 +0900

    add: calendar

--- src/app/league/[leagueId]/page.jsx
+++ src/app/league/[leagueId]/page.jsx
@@ -1,10 +1,21 @@
 import { notFound } from "next/navigation";
+import Calendar from "@/component/calendar/calendar";
+
+async function getLeagueMatches(leagueId) {
+
+}
 
 /**
  * @param params { params: { leagueId: string } }
  */
-export default function LeaguePage({ params }) {
+export default async function LeaguePage({ params }) {
     const leagueId = Number(params.leagueId);
+
+    const matches = await getLeagueMatches(leagueId);
+
+    // TODO: Implement converter (match data to calendar event)
+    // TODO: Implement match filter
+    const events = [];
 
     // show 404 page when wrong league id
     if (isNaN(leagueId)) {
@@ -14,6 +25,9 @@ export default function LeaguePage({ params }) {
     return <>
         <main className="container">
             League Page: {leagueId}
+            <div className="container">
+                <Calendar events={ events }/>
+            </div>
         </main>
     </>;
 }
